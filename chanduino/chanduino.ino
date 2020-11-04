801ef47d14c4aa092f71df7e84143edb9c756c18
rebane2001 <rebane2001@gmail.com>
2020-11-05 01:19:18 +0200

    Bump version

--- chanduino/chanduino.ino
+++ chanduino/chanduino.ino
@@ -98,7 +98,7 @@ const char *root_ca = PSTR( \
 // Web client settings
 const int httpPort = 443;
 const char *host = "a.4cdn.org";
-const String useragent = "Chanduino/0.6";
+const String useragent = "Chanduino/0.7";
 
 // Wifi config webserver variables
 WiFiServer server(80);
