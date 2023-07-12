535d0b8cc92ecd93c4fbb8ae25466fd988072c9e
rebane2001 <rebane2001@gmail.com>
2023-07-12 20:23:53 +0300

    Remove unnecessary code

--- chanduino/chanduino.ino
+++ chanduino/chanduino.ino
@@ -54,21 +54,6 @@
 
 #define CHANDUINO_VERSION "0.8"
 
-#ifndef TFT_DISPOFF
-#define TFT_DISPOFF 0x28
-#endif
-
-#ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
-#endif
-
-#define TFT_MOSI            19
-#define TFT_SCLK            18
-#define TFT_CS              5
-#define TFT_DC              16
-#define TFT_RST             23
-
-#define TFT_BL          4  // Display backlight control pin
 #define ADC_EN          14
 #define ADC_PIN         34
 #define BUTTON_1        35
